45d5debaeb83f66f52580c824a7dac0a3bf77121
bexkysx <xu.song@centre.edu>
2019-02-19 16:04:13 -0500

    dlk

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
+var names=["dog","cat","dragon"];
+var list=document.createElement("OL");
 
+names.forEach(function(d)
+{
+  var li=document.createElement("LI");
+  li.innerText=d;
+  list.appendChild(li);
+})
+var body=document.getElementById("l");
+body.innerHTML=list;
