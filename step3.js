e62852ab148d133270040b75c6cc1900d0673c6d
beckysx <47224761+beckysx@users.noreply.github.com>
2019-02-19 20:23:15 -0500

    Create step3.js

---
+++ step3.js
@@ -0,0 +1,11 @@
+var change=function(){
+
+var a=document.getElementById("dog")
+a.innerText="Big dog"
+var b=document.getElementById("cat")
+b.innerText="Big cat"
+var c=document.getElementById("dragon")
+c.innerText="Big dragon"
+
+
+}
