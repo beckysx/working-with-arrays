1fb710246d7b131d3c0c2da8f2c6a29c3a06155e
bexkysx <xu.song@centre.edu>
2019-02-19 22:40:31 -0500

    step2

--- step2(1).js
+++ step2(1).js
@@ -3,7 +3,9 @@ var change = function() {
   var a = document.getElementsByTagName('li')
   let b = Array.from(a)
   b.forEach(function(d,index) {
-    b[index]=b[index].toString()
+    b[index]=b[index].outerText;
+    alert(b[index])
+
     a[index].innerHTML=b[index].length
 
 
